4010dd0c6d1307710f8d3abb9ce302a005dd3bb1
Aida <118523650+11thmoonlight@users.noreply.github.com>
2024-08-28 22:31:51 +0430

    implement the complete layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,27 @@
+import MobileNavbar from "@/components/MobileNavbar";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main></main>;
+  const loggedIn = { firstName: "Aida", lastName: "Saeedi" };
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div className="">
+            <MobileNavbar user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,36 @@
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 export default function Home() {
-  return <div>page</div>;
+  const loggedIn = {
+    firstName: "Aida",
+    lastName: "Saeedi",
+    email: "aidasaeedi1998@gmail.com",
+  };
+
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 123.5 }]}
+      />
+    </section>
+  );
 }
